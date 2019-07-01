4a804308c4b47ee6000795e3948fb795b4729821
rsaksham <sakshamraghuvanshi1@gmail.com>
2019-07-02 00:19:04 +0530

    error fixed

--- snake.js
+++ snake.js
@@ -13,6 +13,22 @@ ground.src = "img/ground.png";
 const foodImg = new Image();
 foodImg.src = "img/food.png";
 
+// load audio files
+
+const dead = new Audio();
+const eat = new Audio();
+const up = new Audio();
+const left = new Audio();
+const right = new Audio();
+const down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+left.src = "audio/left.mp3";
+right.src = "audio/right.mp3";
+down.src = "audio/down.mp3";
+
 // create snake
 
 let snake = [];
@@ -37,15 +53,29 @@ let d;
 document.addEventListener("keydown", direction);
 
 function direction(event) {
-  if (event.keyCode == 37) {
+  if (event.keyCode == 37 && d != "RIGHT") {
+    left.play();
     d = "LEFT";
-  } else if (event.keyCode == 38) {
+  } else if (event.keyCode == 38 && d != "DOWN") {
+    up.play();
     d = "UP";
-  } else if (event.keyCode == 39) {
+  } else if (event.keyCode == 39 && d != "LEFT") {
+    right.play();
     d = "RIGHT";
-  } else if (event.keyCode == 40) {
+  } else if (event.keyCode == 40 && d != "UP") {
+    down.play();
     d = "DOWN";
   }
+}
+
+// check collision funciton
+function collision(head, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (head.x == array[i].x && head.y == array[i].y) {
+      return true;
+    }
+  }
+  return false;
 }
 
 //draw everything to canvas
@@ -67,14 +97,25 @@ function draw() {
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  //   remove the tail
-  snake.pop();
-
   // which direction
   if (d == "LEFT") snakeX -= box;
   if (d == "UP") snakeY -= box;
   if (d == "RIGHT") snakeX += box;
   if (d == "DOWN") snakeY += box;
+
+  // if snake eats the food
+  if (snakeX == food.x && snakeY == food.y) {
+    score++;
+    eat.play();
+    food = {
+      x: Math.floor(Math.random() * 17 + 1) * box,
+      y: Math.floor(Math.random() * 15 + 3) * box
+    };
+    // we dont remove the tail
+  } else {
+    //   remove the tail
+    snake.pop();
+  }
 
   //   add  new head
 
@@ -82,6 +123,18 @@ function draw() {
     x: snakeX,
     y: snakeY
   };
+
+  // game over
+  if (
+    snakeX < box ||
+    snakeX > 17 * box ||
+    snakeY < 3 * box ||
+    snakeY > 17 * box ||
+    collision(newHead, snake)
+  ) {
+    clearInterval(game);
+    dead.play();
+  }
 
   snake.unshift(newHead);
 
